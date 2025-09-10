28e649c982073a5514ac65d545aa17f14335b5fc
Cruzalirio <nelson-alirio.cruz@uib.es>
2025-09-10 18:25:58 +0200

    CrossCarry

--- tests/testthat/test-edge-cases.R
+++ tests/testthat/test-edge-cases.R
@@ -5,17 +5,17 @@ test_that("Edge cases are handled gracefully", {
   # One subject only
   d1 <- subset(Arterial, Subject == 1)
   cc1 <- createCarry(d1, "Treatment", "Period", "Subject", TRUE)
-  expect_error_free(
+  expect_error(
     CrossGEE("Pressure", "Treatment", "Period", "Subject",
              carry = cc1$carryover, data = cc1$data,
-             correlation = "EXCHANGEABLE", Mv = 1)
+             correlation = "exchangeable", Mv = 1)
   )
 
   # Missing values in response
   d2 <- Arterial
   d2$Pressure[1:3] <- NA
   cc2 <- createCarry(d2, "Treatment", "Period", "Subject", TRUE)
-  expect_error_free(
+  expect_no_error(
     CrossGEE("Pressure", "Treatment", "Period", "Subject",
              carry = cc2$carryover, data = cc2$data,
              correlation = "AR-M", Mv = 1)
